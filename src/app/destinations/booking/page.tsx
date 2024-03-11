13a9e72b542587f66381c87b685aa8af29030bc3
qarani-m <emqarani@gmail.com>
2024-03-11 23:54:19 +0300

    yet another commit

--- src/app/destinations/booking/page.tsx
+++ src/app/destinations/booking/page.tsx
@@ -8,6 +8,10 @@ function page() {
     style={{ height: "auto"}}>
         <NavBar/>
 
+        <div>
+        <CompleteBooking/>
+        </div>
+
         <div style={{marginTop:"10vh"}}>
         <FooterSection/>
       </div>
@@ -15,5 +19,25 @@ function page() {
     </div>
   )
 }
+
+
+
+
+
+
+
+const CompleteBooking = () => {
+  return (
+    <div style={{ display: 'flex', alignItems: 'center', justifyContent:"center",padding: '10px', backgroundColor: '#f5f5f5', flexDirection:"column" }}>
+      <h2 style={{ margin: 0, color: '#333333', fontWeight: 'normal', fontSize:"2.5rem"}}>
+        Complete <span style={{ color: '#ff9900' }}>Booking</span>
+      </h2>
+      <p style={{fontSize:"1.4rem", marginTop: "5vh", marginBottom: "5vh", marginLeft: '10px', color: '#333333', fontWeight: 'normal' }}>12 DAYS KENYA & TANZANIA COMBO MIDRANGE SAFARI</p>
+    </div>
+  );
+};
+
+
+
 
 export default page
